de9d11a56102b752bee30177e87a3438491a60f9
aikrooz <aikrooz>
2025-12-19 22:59:27 +0100

    Implement EntryModelView and set up initial URL routing for posts

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import EntryModel
+from django.views.generic import ListView,DetailView
 # Create your views here.
+class EntryModelView(ListView):
+    model=EntryModel
+    context_object_name="post"
+    template_name="post.html"
+    queryset=EntryModel.uploaded.all()
