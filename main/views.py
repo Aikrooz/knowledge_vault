6397874a660402dbc03a1301c9f59b140a78d948
aikrooz <aikrooz>
2025-12-19 23:46:59 +0100

    Add pagination to EntryModelView and update template file extensions

--- main/views.py
+++ main/views.py
@@ -7,9 +7,10 @@ class EntryModelView(ListView):
     context_object_name="post"
     template_name="post.html"
     queryset=EntryModel.uploaded.all()
+    pagination=5
 
 class EntryModelViewDetail(DetailView):
     model=EntryModel
     context_object_name="post"
-    template_name="post_detail"
+    template_name="post_detail.html"
     queryset=EntryModel.uploaded.all()
